7c9bd592de5b1eb6f0f24a49ef8290d3358c3e11
kks2140501 <kks2140501@naver.com>
2024-11-12 23:29:55 +0900

    데이터 가져오기

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,29 @@ import { API_URL } from "../contants";
 import axios from "axios";
 
 interface ContentType {
-  data: string;
+  seq: number;
+  title: string;
+  dept: string;
+  date: string;
+  views: number;
 }
 
 const getData = async (API_URL: string) => {
   try {
     const html = await axios.get(API_URL);
     const $ = cheerio.load(html.data);
-    // const BB = $(".txtL a").text();
-    const BB = $("tbody").eq(1).text();
-    return BB;
+    let content: ContentType[] = [];
+    const BB = $("tbody").eq(1).find("tr");
+    BB.map((idx, el) => {
+      content[idx] = {
+        seq: parseInt($(el).find("td:nth-child(1)").text()),
+        title: $(el).find("td:nth-child(2)").text(),
+        dept: $(el).find("td:nth-child(3)").text(),
+        date: $(el).find("td:nth-child(4)").text(),
+        views: parseInt($(el).find("td:nth-child(5)").text()),
+      };
+    });
+    return content;
   } catch (err) {
     console.log(err);
   }
@@ -20,5 +33,16 @@ const getData = async (API_URL: string) => {
 
 export default async function Home() {
   const data = await getData(API_URL);
-  return <div>{data}</div>;
+  // jsquery map이랑 javascript의 map은 다르다
+  return (
+    <>
+      {data.map((a, i) => (
+        <div>
+          <span>{a.seq}</span>
+          <h3 key={i}>{a.title}</h3>
+        </div>
+      ))}
+    </>
+  );
+  //return <div>{data}</div>;
 }
